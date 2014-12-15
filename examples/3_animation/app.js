4027273700afb54920fc0997c27dab891b55c6be
piotr.blaszczak <piotr.blaszczak@nf.pl>
2014-12-15 11:38:57 +0100

    animacje demo

---
+++ examples/3_animation/app.js
@@ -0,0 +1,34 @@
+var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state('welcome', {
+            url: "/",
+            templateUrl: "views/welcome.html"
+        })
+        .state('products', {
+            url: "/products",
+            templateUrl: "views/products.html",
+            controller: 'products'
+        })
+        .state('products.product', {
+            url: "/product/:id",
+            templateUrl: "views/product.html",
+            controller: 'product'
+        })
+});
+
+myApp.controller('products', function($scope) {
+    $scope.products = [
+        {id:1, name: 'AngularJS'},
+        {id:2, name: 'ReactJS'},
+        {id:3, name: 'EmberJS'},
+    ];
+})
+
+myApp.controller('product', function($scope,$filter, $stateParams) {
+    $scope.id = $stateParams.id;
+    $scope.product = $filter('filter')($scope.products, $stateParams.id)[0];
+})
